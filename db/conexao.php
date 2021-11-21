6659d106c2e549b516c62364f8e05366915234bf
Gabriel <gboeira777@gmail.com>
2021-11-21 02:37:28 -0300

    Adicionando credenciais heroku

--- db/conexao.php
+++ db/conexao.php
@@ -2,9 +2,16 @@
 class Connection
 {
 
+    //HEROKU
     public static function connectionDB()
     {
-        $conn_string  = pg_connect("host=localhost dbname=papersql user=postgres password=123456");
+        $host = 'ec2-35-168-80-116.compute-1.amazonaws.com';
+        $dbname = 'da889bcon6gnub';
+        $user = 'lntmtgamotyunl';
+        $password = '1e70574a7481af5a2af733f4c5bf33372b060881e494b3523abbbe88ac09a62f';
+        $port = '5432';
+
+        $conn_string  = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
         if (!$conn_string) {
             echo "Error " . pg_last_error();
@@ -12,4 +19,23 @@ class Connection
 
         return $conn_string;
     }
+
+
+    // //LOCALHOST
+    // public static function connectionDB()
+    // {
+    //     $host = 'localhost';
+    //     $dbname = 'papersql';
+    //     $user = 'postgres';
+    //     $password = '123456';
+    //     $port = '5432';
+
+    //     $conn_string  = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
+    //     if (!$conn_string) {
+    //         echo "Error " . pg_last_error();
+    //     }
+
+    //     return $conn_string;
+    // }
 }
