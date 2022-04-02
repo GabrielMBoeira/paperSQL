2cef71bbd0aac87c9a5b0e23b90a7af177d9cc0d
Gabriel <gboeira777@gmail.com>
2022-04-01 22:50:29 -0300

    limpando conexao

--- db/conexao.php
+++ db/conexao.php
@@ -1,15 +1,13 @@
 <?php
 class Connection
 {
-
-    //HEROKU
     public static function connectionDB()
     {
-        $host = 'ec2-35-168-80-116.compute-1.amazonaws.com';
-        $dbname = 'da889bcon6gnub';
-        $user = 'lntmtgamotyunl';
-        $password = '1e70574a7481af5a2af733f4c5bf33372b060881e494b3523abbbe88ac09a62f';
-        $port = '5432';
+        $host = 'localhost';
+        $dbname = '';
+        $user = '';
+        $password = '';
+        $port = '';
 
         $conn_string  = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
@@ -19,23 +17,4 @@ class Connection
 
         return $conn_string;
     }
-
-
-    // //LOCALHOST
-    // public static function connectionDB()
-    // {
-    //     $host = 'localhost';
-    //     $dbname = 'papersql';
-    //     $user = 'postgres';
-    //     $password = '123456';
-    //     $port = '5432';
-
-    //     $conn_string  = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
-
-    //     if (!$conn_string) {
-    //         echo "Error " . pg_last_error();
-    //     }
-
-    //     return $conn_string;
-    // }
 }
