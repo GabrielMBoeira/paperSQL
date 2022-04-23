505f8b2b27d1986c20d2d15ab6bd4f960c229365
Gabriel Machado Boeira <gabriel.boeira@softplan.com.br>
2022-04-23 14:00:33 -0300

    Criação tela agendar cadastrar agenda

---
+++ cad_horario.php
@@ -0,0 +1,30 @@
+<?php
+require_once('template/header.php');
+?>
+
+<link rel="stylesheet" href="css/cad_horario.css">
+
+<main>
+    <div class="content-form">
+        <form>
+            <div class="title-form col-md-12 d-flex justify-content-center align-itens-center fs-2">Cadastrar horário</div>
+            <div class="col-md-12 mt-4">
+                <label for="data" class="form-label">Data:</label>
+                <input type="date" class="form-control" placeholder="Insira uma data...">
+            </div>
+
+            <div class="col-md-12 mt-4">
+                <label for="horario" class="form-label">Horario:</label>
+                <input type="time" class="form-control" placeholder="Insira uma horario...">
+            </div>
+
+            <div class="content-button d-flex justify-content-end mt-4">
+                <button class="btn btn-primary">Salvar</button>
+            </div>
+        </form>
+    </div>
+</main>
+
+<?php
+require_once('template/footer.php');
+?>
--- index.php
+++ index.php
@@ -1,23 +1,28 @@
 <?php
-require_once('template/header.php');
-require_once('db/conexao.php');
+// require_once('db/conexao.php');
 ?>
 
 <div class="cont">
+    <!-- Importação de template cabeçaalho -->
+    <?php require_once('template/header.php'); ?>
+    
     <div class="clock-content">
         <div class="clock" id="clock"></div>
     </div>
-    <div class="div1">
-        <button class="btn1">Agendar Horário</button>
+    
+    <div class="content-center">
+        <div class="div1">
+            <button class="btn1">Agendar Horário</button>
+        </div>
+        
+        <div class="div2">
+            <button class="btn2 ">Consultar Horário</button>
+        </div>
     </div>
-    <div class="div2">
-        <button class="btn2">Consultar Horário</button>
-    </div>
+    
+    <!-- Importação de template rodapé -->
+    <?php require_once('template/footer.php') ?>
 </div>
 
 
 <script src="js/function.js"></script>
-
-<?php
-require_once('template/footer.php')
-?>
