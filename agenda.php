bf9d0b83f8f0d74867e53dad1989a749620289d9
Gabriel Machado Boeira <gabriel.boeira@softplan.com.br>
2022-04-24 19:41:50 -0300

    Ajuste css tela agenda

--- agenda.php
+++ agenda.php
@@ -9,32 +9,36 @@ require_once('template/header.php');
         <form class="row g-3">
             <div class="title-form col-md-12 d-flex justify-content-center align-itens-center fs-2">Agenda</div>
 
-            <table class="table table-dark table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">Data</th>
-                        <th scope="col">Hora</th>
-                        <th scope="col">Nome</th>
-                        <th scope="col">CPF</th>
-                        <th scope="col">Editar</th>
-                        <th scope="col">Excluir</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">25/04/2022</th>
-                        <td>08:00</td>
-                        <td>Carlos da Silva</td>
-                        <td>023.456.432-97</td>
-                        <td>
-                            <button class="btn btn-sm btn-success"><i class="fa-solid fa-alicorn"></i>Editar</button>
-                        </td>
-                        <td>
-                            <button class="btn btn-sm btn-danger">Excluir</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table class="table table-dark table-hover table-responsive border border-light">
+                    <thead>
+                        <tr>
+                            <th scope="col">Data</th>
+                            <th scope="col">Hora</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col ">CPF</th>
+                            <th scope="col">Editar</th>
+                            <th scope="col">Excluir</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th class="text-truncate">25/04/2022</th>
+                            <td class="text-truncate">08:00</td>
+                            <td class="text-truncate">Carlos da Silva</td>
+                            <td class="text-truncate">023.456.432-97</td>
+                            <td class="text-truncate">
+                                <button class="btn btn-sm btn-success"><i class="fa-solid fa-alicorn"></i>Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Excluir</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
+
 
         </form>
     </div>
