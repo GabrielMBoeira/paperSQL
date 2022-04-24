401e49da0b46d3d93a1fe1ad1d03c9dc1c4fddc7
Gabriel Machado Boeira <gabriel.boeira@softplan.com.br>
2022-04-24 18:50:30 -0300

    Layout provisorio

--- index.php
+++ index.php
@@ -5,11 +5,23 @@ require_once('template/header.php');
 <link rel="stylesheet" href="css/style.css">
 
 <main>
-    <div class="content-form text-light">
-        Em manutenção
+    <div class="content-clock">
+        <div class="clock" id="clock"></div>
     </div>
+
+    <div class="content-center">
+        <div class="div1">
+            <button class="btn1">Agendar Horário</button>
+        </div>
+
+        <div class="div2">
+            <button class="btn2">Consultar Horário</button>
+        </div>
+    </div> 
 </main>
 
 <?php
 require_once('template/footer.php');
 ?>
+
+<script src="js/function.js"></script>
